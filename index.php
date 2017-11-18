d0bda2aac48baf04f63b8cb740621327342804d4
danbond85@mail.ru <danbond85@gmail.com>
2017-11-18 13:57:54 +0300

    All classes

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ class Car
 {           
     const WHEEL_COUNT = 4;
 
-    private $wheelCount = WHEEL_COUNT;
+    private $wheelCount = self::WHEEL_COUNT;
     private $color = [255, 255, 255];
     private $speed = 0;
 
@@ -41,7 +41,7 @@ class Car
         if (in_array($r, range(0,255)) or 
             in_array($g, range(0,255)) or
             in_array($b, range(0,255))) {
-            echo 'Все компоненты цвета должны быть в диапазоне от 0 до 255';
+            echo 'Все компоненты цвета должны быть в диапазоне от 0 до 255'.PHP_EOL;
         }
         else {
             $this->color = $color;
@@ -51,13 +51,13 @@ class Car
     public function takeOffWheels($wheelCount)
     {
         if (!$this->wheelCount) {
-            echo 'На машине не установлены колёса';
+            echo 'На машине не установлены колёса'.PHP_EOL;
         }
-        elseif ($wheelCount > WHEEL_COUNT) {
-            echo 'У машины не бывает столько колёс';
+        elseif ($wheelCount > self::WHEEL_COUNT) {
+            echo 'У машины не бывает столько колёс'.PHP_EOL;
         }
         elseif ($wheelCount > $this->wheelCount) {
-            echo 'Вы пытаетесь снять колёс больше, чем установлено на машине. Установлено: '.$this->wheelCount;
+            echo 'Вы пытаетесь снять колёс больше, чем установлено на машине. Установлено: '.$this->wheelCount.PHP_EOL;
         }
         else {
             $this->wheelCount -= $wheelCount;
@@ -66,11 +66,11 @@ class Car
 
     public function takeOnWheels($wheelCount)
     {
-        if ($this->wheelCount == WHEEL_COUNT) {
-            echo 'На машине установлены все колёса';                
+        if ($this->wheelCount == self::WHEEL_COUNT) {
+            echo 'На машине установлены все колёса'.PHP_EOL;                
         }
-        elseif ($wheelCount + $this->wheelCount > WHEEL_COUNT) {
-            echo 'Вы пытаетесь установить колёс больше, чем может быть в машине. Установлено: '.$this->wheelCount;
+        elseif ($wheelCount + $this->wheelCount > self::WHEEL_COUNT) {
+            echo 'Вы пытаетесь установить колёс больше, чем может быть в машине. Установлено: '.$this->wheelCount.PHP_EOL;
         }
         else {
             $this->wheelCount += $wheelCount;
@@ -145,14 +145,14 @@ class TvSet
 
     public function clickTune($channels)
     {
-        if ($powerOn) {
+        if ($this->$powerOn) {
             $this->channels = array_merge($this->channels, $channels);
         }
     }
 
     public function clickVolumePlus($time)
     {
-        if ($powerOn) {
+        if ($this->$powerOn) {
             $newVolume = $this->volume + $time;
             $this->volume = ($newVolume < 100) ? $newVolume : 100;
         }
@@ -160,7 +160,7 @@ class TvSet
 
     public function clickVolumeMinus($time)
     {
-        if ($powerOn) {
+        if ($this->$powerOn) {
             $newVolume = $this->volume - $time;
             $this->volume = ($newVolume > 0) ? $newVolume : 0;
         }
@@ -168,7 +168,7 @@ class TvSet
 
     public function clickChannelPlus()
     {
-        if ($powerOn) {
+        if ($this->$powerOn) {
             if ($this->currentChannel == count($this->channels) - 1) {
                 $this->currentChannel = 0;
             }
@@ -180,7 +180,7 @@ class TvSet
 
     public function clickChannelMinus()
     {
-        if ($powerOn) {
+        if ($this->$powerOn) {
             if ($this->currentChannel == 0) {
                 $this->currentChannel = count($this->channels) - 1;
             }
@@ -192,3 +192,184 @@ class TvSet
 }
 
 
+class Pen 
+{
+    const PT_INK_RATE = 0.01; //per cent of ink level, spent on 1 char pt
+
+    private $color;
+    private $inkLevel = 100;
+
+    public function __construct($color = [0, 0, 255])
+    {
+        $this->color = $color;
+    }
+
+    public function getInkLevel()
+    {
+        return $this->inkLevel;
+    }
+
+    /*
+    * Decreasing ink level depending on written chars and font size (in pt)
+    */
+    public function write($chars, $fontSize)
+    {
+        if ($this->inkLevel > 0) {
+            $inkRate = self::PT_INK_RATE * $chars * $fontSize;
+
+            if ($this->inkLevel > $inkRate) {
+                $this->inkLevel = round($this->inkLeve - $inkRate, 2); // converting floating point to fixed point
+            }
+            else {
+                $possibleChars = floor($this->inkLevel / self::PT_INK_RATE * $fontSize );
+
+                echo "Невозможно написать $chars символов $fontSize-м шифтом оставшимся количеством чернил. Написано $possibleChars символов".PHP_EOL;
+            }
+        }
+    }
+
+    public function recharge()
+    {
+        $this->inkLevel = 100;
+    }
+}
+
+class Duck 
+{
+    private $color;
+    private $weight;
+
+    public function __construct($color = [255, 255, 0], $weight = 100) // Standard yellow rubber duck
+    {
+        $this->color = $color;
+        $this->weight = $weight;
+    }
+
+    public function getColor()
+    {
+        return $this->color;
+    }
+
+    public function getWeigth()
+    {
+        return $this->weight;
+    }
+
+    public function press()
+    {
+        echo "Quack!".PHP_EOL;
+    }
+}
+
+class Product
+{
+    private $name;
+    private $description;
+    private $category;
+    private $weight = 100; // Standard weight
+    private $price = 0;    // Standard price
+    private $discount = 0; // Standard discount
+
+    public function __construct(
+        $name, 
+        $weight, 
+        $price, 
+        $description = "", 
+        $category = "Uncategorized", 
+        $discount = 0)
+    {
+        $this->name = $name;
+        $this->description = $description;
+        $this->category = $category;
+
+        $this->setWeight($weight);
+        $this->setPrice($price);
+        $this->setDiscount($discount);
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    public function getCategory()
+    {
+        return $this->category;
+    }
+
+    public function getWeight()
+    {
+        return $this->weight;
+    }
+
+    public function getPrice()
+    {
+        $price = $round($this->price * (1 - 0.01 * ($this->discount)), 2);
+
+        return $price;
+    }
+
+    public function getDiscount()
+    {
+        return $this->discount;
+    }
+
+    public function setDescription($description) {
+        $this->description = $description;
+    }
+
+    public function setCategory($category) {
+        $this->category = $category;
+    }
+
+    public function setPrice($price) {
+        if ($price > 0) {
+            $this->price = $price;
+        }
+        else {
+            echo 'Цена не может быть отрицательной. Задана цена по умолчанию: '.$this->price.PHP_EOL;
+        }
+    }
+
+    public function setDiscount($discount)
+    {
+        if ($discount >= 0 and $discount < 100) {
+            $this->discount = $discount;
+        }
+        else {
+            echo 'Скидка может быть в пределах от 0 до 100 (не включительно). Задана скидка по умолчанию: '.$this->discount.PHP_EOL;
+        }
+    }
+
+    public function setWeight($weight)
+    {
+        if ($weight > 0) {
+            $this->weight = $weight;
+        }
+        else {
+            echo 'Вес не может быть отрицательным. Задан вес по умолчанию: '.$this->weight.PHP_EOL;
+        }
+    }
+}
+
+
+/* ==== Main code ==== */
+$silverCar = new Car([192, 192, 192]);
+$scarletCar = new Car([255, 36, 0]);
+
+$tvSet1 = new TvSet('TV00001');
+$tvSet2 = new TvSet('TV00002');
+
+$bluePen = new Pen();
+$greenPen = new Pen([0, 255, 0]);
+
+$standardYellowDuck = new Duck();
+$hugeCyanDuck = new Duck([0, 255, 255], 3000);
+
+$iPhone = new Product('iPhone X', 174, 74999, 'The smartphone of future.', 'Smartphones', 0);
+$ps4Pro = new Product('Playstation 4 Pro', 3300, 29999, 'Greatness awaits.', 'Videogame consoles', 10);
